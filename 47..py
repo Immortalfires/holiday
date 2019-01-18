3fa859190c873e23c0d83bdabe02eecba4e058e6
Immortalfires <2247797673@qq.com>
2019-01-18 12:26:30 +0800

    46.47.48

---
+++ 47..py
@@ -0,0 +1,8 @@
+x=int(input())
+while(x):
+         mi=x**2
+         if(mi<50):
+                  break
+         else:
+                  print(mi)
+                  x=int(input())
---
+++ 48..py
@@ -0,0 +1,6 @@
+a=int(input())
+b=int(input())
+print("a=%d,b=%d"%(a,b))
+a,b=b,a
+print("a=%d,b=%d"%(a,b))
+      
