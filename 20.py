9d6ba068b511354034d6502eb7dce4bc46fafb07
Immortalfires <2247797673@qq.com>
2019-01-18 12:17:56 +0800

    20

---
+++ 20.py
@@ -0,0 +1,10 @@
+n=int(input())
+for n in range(0,n+1):
+                  if (n%3==0):
+                           print("Fizz")
+                  if(n%5==0):
+                           print("Buzz")
+                  if(n%3==0 and n%5==0):
+                           print("FizzBuzz")
+         
+         
